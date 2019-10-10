864e6e1404f1ec79ab270c09db2ba08183ad2ddf
Aymen chaari <aymenchaari@gmail.com>
2019-10-10 11:22:33 +0100

    commit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { LogComponent } from './log/log.component';
+import { Test3Component } from './test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    LogComponent
+    LogComponent,
+    Test3Component
   ],
   imports: [
     BrowserModule,

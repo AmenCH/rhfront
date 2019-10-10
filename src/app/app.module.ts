4a04b69061cc44cbb8c05ef9fd3935750de7f15c
Aymen chaari <aymenchaari@gmail.com>
2019-10-10 10:59:47 +0100

    addLog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
